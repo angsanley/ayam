a2e86c17e22ea3ec7e644b2a5c18b28225a6cb6f
Stanley <angstanley68@gmail.com>
2020-05-20 06:02:08 +0700

    Refactoring

---
+++ src/components/Assignments.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h3 class="mb-2">Assignments</h3>
+        <div class="items">
+            <div class="card assignment-card flex flex-shrink-0" v-for="assignment in this.assignments" :key="assignment.Title">
+                <div class="flex flex-col h-full justify-between">
+                    <h5>{{courses.find(e => e.CLASS_NBR === assignment.classNbr).COURSENAME}}</h5>
+                    <div class="text-sm text-gray-500">{{assignment.Title}}</div>
+                    <div class="pt-2 text-sm text-gray-500 mt-auto">Due {{assignment.deadlineDuration | relativeTime}}</div>
+                    <!--                        <a :href="`https://binusmaya.binus.ac.id/services/ci/index.php/general/downloadDocument/${assignment.assignmentPathLocation}`">Download file</a>-->
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import moment from "moment";
+
+    export default {
+        name: "Assignments",
+        props: ['assignments', 'courses'],
+        filters: {
+            relativeTime: function (date) {
+                return moment(date).fromNow();
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .assignment-card {
+        @apply w-64 inline-block p-4 mr-4;
+        white-space: normal;
+        word-wrap: break-word !important;
+    }
+
+    .items {
+        white-space: nowrap;
+        overflow-x: auto;
+        @apply pl-2 pt-2 pb-8 flex;
+    }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,17 +6,7 @@
         <!--        </div>-->
 
         <div class="w-full">
-            <h3 class="mb-2">Assignments</h3>
-            <div class="items">
-                <div class="card assignment-card flex flex-shrink-0" v-for="assignment in this.assignments" :key="assignment.Title">
-                    <div class="flex flex-col h-full justify-between">
-                        <h5>{{courses.find(e => e.CLASS_NBR === assignment.classNbr).COURSENAME}}</h5>
-                        <div class="text-sm text-gray-500">{{assignment.Title}}</div>
-                        <div class="pt-2 text-sm text-gray-500 mt-auto">Due {{assignment.deadlineDuration | relativeTime}}</div>
-<!--                        <a :href="`https://binusmaya.binus.ac.id/services/ci/index.php/general/downloadDocument/${assignment.assignmentPathLocation}`">Download file</a>-->
-                    </div>
-                </div>
-            </div>
+            <assignments :assignments="assignments" :courses="courses" v-if="assignments.length > 0"/>
         </div>
     </div>
 </template>
@@ -30,20 +20,6 @@
         @apply uppercase text-gray-700 font-bold;
     }
 
-    .assignment-card {
-        @apply w-64 inline-block p-4 mr-4;
-        white-space: normal;
-        word-wrap: break-word !important;
-    }
-
-    .items {
-        white-space: nowrap;
-        overflow-x: auto;
-        @apply pl-2 pt-2 pb-8 flex;
-    }
-
-    .items > div {
-    }
 </style>
 
 <script>
@@ -56,10 +32,11 @@
     import axios from "axios";
     import * as HtmlTableToJson from "html-table-to-json";
     import moment from "moment";
+    import Assignments from "../components/Assignments";
 
     export default {
         name: 'Home',
-        components: {},
+        components: {Assignments},
         data() {
             return {
                 binusianData: {},
