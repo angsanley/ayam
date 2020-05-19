f22894905dc74a364caf34f2246c5691f2f97a37
Stanley <angstanley68@gmail.com>
2020-05-19 15:25:03 +0700

    Add other essentials

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueTailwind from 'vue-tailwind'
+import './assets/css/main.css'
+import router from './router'
+import store from './store'
 
 Vue.use(VueTailwind)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
