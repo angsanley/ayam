7e26ec2ae07245a310846573c32af73ec1a914d8
Stanley <angstanley68@gmail.com>
2020-05-29 04:17:28 +0700

    Update assignments and VC components

--- src/components/Assignments.vue
+++ src/components/Assignments.vue
@@ -4,25 +4,30 @@
             <h3 class="mb-2">Upcoming Assignments</h3>
             <router-link class="transform hover:scale-105 hover:bg-white dark-mode:hover:bg-gray-800 hover:shadow-lg transition ease-out duration-300 py-1 px-2 rounded-lg uppercase text-sm font-bold text-blue-600 no-underline" to="/dashboard/assignments">Show all</router-link>
         </div>
-        <perfect-scrollbar :options="{handlers: ['wheel', 'touch']}" class="items" v-dragscroll="window.width >= 1024">
-            <div class="card assignment-card flex flex-shrink-0" v-for="assignment in this.assignments" :key="assignment.Title">
-                <div class="flex flex-col h-full justify-between">
-                    <h5>{{courses.find(e => e.CLASS_NBR === assignment.classNbr).COURSENAME}}</h5>
-                    <div class="text-sm text-gray-500">{{assignment.Title}}</div>
-                    <div class="pt-2 text-sm text-gray-500 mt-auto">Due {{`${assignment.deadlineDuration} ${assignment.deadlineTime}` | relativeTime}}</div>
-                    <!--                        <a :href="`https://binusmaya.binus.ac.id/services/ci/index.php/general/downloadDocument/${assignment.assignmentPathLocation}`">Download file</a>-->
+        <horizontal-scrolling-container>
+
+            <router-link class="no-underline" to="/dashboard/assignments" v-for="assignment in this.assignments" :key="assignment['Title']">
+                <div class="card assignment-card flex flex-shrink-0">
+                    <div class="flex flex-col h-full justify-between">
+                        <h5>{{courses.find(e => e['CLASS_NBR'] === assignment.classNbr)['COURSENAME']}}</h5>
+                        <div class="text-sm text-gray-500">{{assignment['Title']}}</div>
+                        <div class="pt-2 text-sm text-gray-500 mt-auto">Due {{`${assignment['deadlineDuration']} ${assignment['deadlineTime']}` | relativeTime}}</div>
+                        <!--                        <a :href="`https://binusmaya.binus.ac.id/services/ci/index.php/general/downloadDocument/${assignment.assignmentPathLocation}`">Download file</a>-->
+                    </div>
                 </div>
-            </div>
-        </perfect-scrollbar>
+            </router-link>
+        </horizontal-scrolling-container>
     </div>
 </template>
 
 <script>
     import moment from "moment-timezone";
     import { dragscroll } from 'vue-dragscroll';
+    import HorizontalScrollingContainer from "./HorizontalScrollingContainer";
 
     export default {
         name: "Assignments",
+        components: {HorizontalScrollingContainer},
         props: ['assignments', 'courses'],
         data() {
             return {
--- src/components/VideoConferences.vue
+++ src/components/VideoConferences.vue
@@ -4,28 +4,31 @@
             <h3 class="mb-2">Upcoming Video Conferences</h3>
             <router-link class="transform hover:scale-105 hover:bg-white dark-mode:hover:bg-gray-800 hover:shadow-lg transition ease-out duration-300 py-1 px-2 rounded-lg uppercase text-sm font-bold text-blue-600 no-underline" to="/dashboard/videoconferences">Show all</router-link>
         </div>
-        <perfect-scrollbar :options="{handlers: ['wheel', 'touch']}" class="items" v-dragscroll="window.width >= 1024">
-            <div class="card assignment-card flex flex-shrink-0" v-for="videoConference in this.videoConferences" :key="videoConference.Title">
+
+        <horizontal-scrolling-container>
+            <div class="card assignment-card flex flex-shrink-0" v-for="videoConference in this.videoConferences" :key="videoConference['Title']">
                 <div class="flex flex-col h-full justify-between">
                     <div>
-                        <h5>{{courses.find(e => e.CLASS_NBR === videoConference.classNbr).COURSENAME}}</h5>
-                        <div class="text-sm font-medium text-gray-600">{{courses.find(e => e.CLASS_NBR === videoConference.classNbr).SSR_COMPONENT}}</div>
+                        <h5>{{courses.find(e => e.CLASS_NBR === videoConference.classNbr)['COURSENAME']}}</h5>
+                        <div class="text-sm font-medium text-gray-600">{{courses.find(e => e['CLASS_NBR'] === videoConference.classNbr)['SSR_COMPONENT']}}</div>
                     </div>
                     <div class="pt-2 text-sm text-gray-500 mt-auto">Starts {{videoConference.startDate | relativeTime}}</div>
-                    <a class="button default-button mt-2 text-center no-underline text-sm" :href="videoConference.Link"><i class="fas fa-external-link-alt"/> Open in Zoom</a>
+                    <a class="button default-button mt-2 text-center no-underline text-sm" :href="videoConference['Link']" target="_blank"><i class="fas fa-external-link-alt"/> Open in Zoom</a>
                     <!--                        <a :href="`https://binusmaya.binus.ac.id/services/ci/index.php/general/downloadDocument/${assignment.assignmentPathLocation}`">Download file</a>-->
                 </div>
             </div>
-        </perfect-scrollbar>
+        </horizontal-scrolling-container>
     </div>
 </template>
 
 <script>
     import moment from "moment-timezone";
     import { dragscroll } from 'vue-dragscroll';
+    import HorizontalScrollingContainer from "./HorizontalScrollingContainer";
 
     export default {
         name: "VideoConferences",
+        components: {HorizontalScrollingContainer},
         props: ['videoConferences', 'courses'],
         data() {
             return {
